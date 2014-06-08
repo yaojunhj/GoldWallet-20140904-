82b234d11fa331e389fee0620370de6b71729975
felecia <felecia@feleciatekiMacBook-Pro.local>
2014-06-08 17:25:11 +0800

    Initial Commit

---
+++ GoldWallet/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  GoldWallet
+//
+//  Created by felecia on 14-6-8.
+//  Copyright (c) 2014年 JinDing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoldWallet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GoldWallet
+//
+//  Created by felecia on 14-6-8.
+//  Copyright (c) 2014年 JinDing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
